9bb3b53e9c7a8a9163e586ff4135fdb6027d8bd4
Ticoiu Lucian <lucian.ticoiu.ext@alten.com>
2024-09-11 17:30:11 +0300

    Initial commit

---
+++ src/main/java/ro/ftc/FtcApplication.java
@@ -0,0 +1,13 @@
+package ro.ftc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtcApplication.class, args);
+	}
+
+}
