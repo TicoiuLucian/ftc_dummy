c8dfcf6cd3313509f2553b4a36190fb12ef28daa
Iulia Filimon <the.doctor.beep@gmail.com>
2024-09-15 23:16:02 +0300

    participant page

--- src/main/java/ro/ftc/restcontroller/PersonController.java
+++ src/main/java/ro/ftc/restcontroller/PersonController.java
@@ -1,15 +1,17 @@
 package ro.ftc.restcontroller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ro.ftc.entity.Person;
 import ro.ftc.service.IPersonService;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/person")
+@RequestMapping("/person") //TOATE INCEP CU /PERSON !!!!!!!!!!!!!!!!!!!!! /PERSON/PARTICIPANTS
 public class PersonController {
 
   private final IPersonService personService;
@@ -22,6 +24,13 @@ public class PersonController {
   @PostMapping
   public void save(@RequestBody Person person) {
     personService.save(person);
+  }
+
+  @GetMapping("/participants")
+  public String viewParticipantsPage(final Model model) {
+    List<Person> participants = personService.findAll();
+    model.addAttribute("participants", participants);
+    return "participants";
   }
 
 }
